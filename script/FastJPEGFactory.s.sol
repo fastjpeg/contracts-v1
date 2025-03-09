83e2598c40c5630a645ddb4ce6a35c961d192643
Joe Blau <im@joeblau.com>
2025-03-09 16:18:12 -0700

    feat: fix script

---
+++ script/FastJPEGFactory.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {FastJPEGFactory} from "../src/FastJPEGFactory.sol";
+
+contract FastJPEGFactoryScript is Script {
+    FastJPEGFactory public fastJpegFactory;
+
+    function setUp() public {
+        // fastJpegFactory = new FastJPEGFactory();
+    }
+
+    function run() public {
+        vm.startBroadcast();
+
+        vm.stopBroadcast();
+    }
+}
