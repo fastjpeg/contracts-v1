267957b7570f1670b97d381ea70eaea3588c754d
Joe Blau <im@joeblau.com>
2025-03-17 14:38:47 -0700

    feat: fix token tests

--- test/FastJPEGToken.t.sol
+++ test/FastJPEGToken.t.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.19;
 
 import { Test, console } from "forge-std/Test.sol";
 import { FastJPEGToken } from "../src/FastJPEGToken.sol";
+import { ERC20Capped } from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import { ERC20 } from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import { IERC20Errors } from "../lib/openzeppelin-contracts/contracts/interfaces/draft-IERC6093.sol";
 
 contract FastJPEGTokenTest is Test {
     FastJPEGToken public fastJpegToken;
@@ -19,11 +22,11 @@ contract FastJPEGTokenTest is Test {
         fastJpegToken = new FastJPEGToken("Fast JPEG Token", "FJPG");
     }
 
-    function testDecimals() public {
+    function testDecimals() public view {
         assertEq(fastJpegToken.decimals(), 18);
     }
 
-    function testSupplyCap() public {
+    function testSupplyCap() public view {
         assertEq(fastJpegToken.cap(), 1_000_000_000 * 10 ** 18);
     }
 
@@ -39,12 +42,27 @@ contract FastJPEGTokenTest is Test {
     }
 
     function testMintOverCap() public {
-        vm.expectRevert("ERC20Capped: cap exceeded");
+        // Test that minting over the cap reverts with ERC20ExceededCap error
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ERC20Capped.ERC20ExceededCap.selector,
+                1000000001000000000000000000,
+                1000000000000000000000000000
+            )
+        );
+        // vm.expectRevert("ERC20Capped: cap exceeded");
         fastJpegToken.mint(user1, 1_000_000_001 * 10 ** 18);
     }
 
     function testBurnOverBalance() public {
-        vm.expectRevert("ERC20: burn amount exceeds balance");
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IERC20Errors.ERC20InsufficientBalance.selector,
+                user1,
+                0,
+                1000000001000000000000000000
+            )
+        );
         fastJpegToken.burn(user1, 1_000_000_001 * 10 ** 18);
     }
 }
