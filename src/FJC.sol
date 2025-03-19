c04c6ba7802e01c489f462cf6329a0b4dc1435bf
Joe Blau <im@joeblau.com>
2025-03-19 07:19:00 -0700

    feat: rename token to coin

--- src/FJC.sol
+++ src/FJC.sol
@@ -6,7 +6,7 @@ import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract FastJPEGToken is ERC20, Ownable, ERC20Capped {
+contract FJC is ERC20, Ownable, ERC20Capped {
     constructor(string memory name, string memory symbol)
         ERC20(name, symbol)
         ERC20Capped(1_000_000_000 * 1e18)
--- test/FJC.t.sol
+++ test/FJC.t.sol
@@ -2,46 +2,46 @@
 pragma solidity ^0.8.19;
 
 import { Test, console } from "forge-std/Test.sol";
-import { FastJPEGToken } from "../src/FastJPEGToken.sol";
+import { FJC } from "../src/FastJPEGFactory.sol";
 import { ERC20Capped } from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import { ERC20 } from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import { IERC20Errors } from "../lib/openzeppelin-contracts/contracts/interfaces/draft-IERC6093.sol";
 
-contract FastJPEGTokenTest is Test {
-    FastJPEGToken public fastJpegToken;
-    address public fastJpegOwner;
+contract FastJPEGCoinTest is Test {
+    FJC public fastJpegCoin;
+    address public owner;
     address public user1;
 
     function setUp() public {
-        fastJpegOwner = address(this);
+        owner = address(this);
         user1 = vm.addr(1);
 
-        vm.deal(fastJpegOwner, 0 ether);
+        vm.deal(owner, 0 ether);
         vm.deal(user1, 100 ether);
 
-        fastJpegToken = new FastJPEGToken("Fast JPEG Token", "FJPG");
+        fastJpegCoin = new FJC("Fast JPEG Coin", "FJPC");
     }
 
-    function testDecimals() public view {
-        assertEq(fastJpegToken.decimals(), 18);
+    function test_decimals() public view {
+        assertEq(fastJpegCoin.decimals(), 18);
     }
 
-    function testSupplyCap() public view {
-        assertEq(fastJpegToken.cap(), 1_000_000_000 * 10 ** 18);
+    function test_supplyCap() public view {
+        assertEq(fastJpegCoin.cap(), 1_000_000_000 * 10 ** 18);
     }
 
-    function testMint() public {
-        fastJpegToken.mint(user1, 100_000_000 * 10 ** 18);
-        assertEq(fastJpegToken.balanceOf(user1), 100_000_000 * 10 ** 18);
+    function test_mint() public {
+        fastJpegCoin.mint(user1, 100_000_000 * 10 ** 18);
+        assertEq(fastJpegCoin.balanceOf(user1), 100_000_000 * 10 ** 18);
     }
 
-    function testBurn() public {
-        fastJpegToken.mint(user1, 100_000_000 * 10 ** 18);
-        fastJpegToken.burn(user1, 100_000_000 * 10 ** 18);
-        assertEq(fastJpegToken.balanceOf(user1), 0);
+    function test_burn() public {
+        fastJpegCoin.mint(user1, 100_000_000 * 10 ** 18);
+        fastJpegCoin.burn(user1, 100_000_000 * 10 ** 18);
+        assertEq(fastJpegCoin.balanceOf(user1), 0);
     }
 
-    function testMintOverCap() public {
+    function test_mintOverCap() public {
         // Test that minting over the cap reverts with ERC20ExceededCap error
         vm.expectRevert(
             abi.encodeWithSelector(
@@ -49,15 +49,15 @@ contract FastJPEGTokenTest is Test {
             )
         );
         // vm.expectRevert("ERC20Capped: cap exceeded");
-        fastJpegToken.mint(user1, 1_000_000_001 * 10 ** 18);
+        fastJpegCoin.mint(user1, 1_000_000_001 * 10 ** 18);
     }
 
-    function testBurnOverBalance() public {
+    function test_burnOverBalance() public {
         vm.expectRevert(
             abi.encodeWithSelector(
                 IERC20Errors.ERC20InsufficientBalance.selector, user1, 0, 1000000001000000000000000000
             )
         );
-        fastJpegToken.burn(user1, 1_000_000_001 * 10 ** 18);
+        fastJpegCoin.burn(user1, 1_000_000_001 * 10 ** 18);
     }
 }
