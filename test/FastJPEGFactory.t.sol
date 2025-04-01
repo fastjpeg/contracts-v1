efed6089fb1da1c4bd26c102371b255e1652c9c4
Joe Blau <im@joeblau.com>
2025-04-01 08:10:37 -0700

    feat: gas optimziation on reverts

--- test/FastJPEGFactory.t.sol
+++ test/FastJPEGFactory.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import { Test, console } from "forge-std/Test.sol";
-import { FastJPEGFactory, FJC } from "../src/FastJPEGFactory.sol";
+import { FastJPEGFactory, FJC, FastJPEGFactoryError } from "../src/FastJPEGFactory.sol";
 import { IERC20 } from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import { IUniswapV2Factory } from "../lib/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import { IUniswapV2Router02 } from "../lib/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
@@ -268,7 +268,7 @@ contract FastJPEGFactoryTest is Test {
 
     // expect error if recieptos is 0 and percentage is not 0
     function test_createTokenAirdropNoRecipientsAndPercentageNot0() public {
-        vm.expectRevert("Airdrop percentage must be 0 when no recipients");
+        vm.expectRevert(abi.encodeWithSelector(FastJPEGFactoryError.AirdropPercentageMustBeZero.selector));
         factory.newCoinAirdrop(coinName, coinSymbol, new address[](0), 1000, testMetadataHash);
     }
 
